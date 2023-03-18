fde4934796e0b0966173f5606a60adf9faa7f24b
Alhumam Jabakhanji <alhumam567@gmail.com>
2023-03-17 22:56:08 -0400

    Reading frame header and data

--- mp3_metadata.c
+++ mp3_metadata.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct ID3V2_HEADER {
     char fid[3];
@@ -7,6 +8,16 @@ typedef struct ID3V2_HEADER {
     char flags;
     char size[4];
 } ID3V2_HEADER;
+
+typedef struct ID3V2_FRAME_HEADER {
+    char fid[4+1];
+    char size[4];
+    char flags[2];
+} ID3V2_FRAME_HEADER;
+
+int synchsafeint32ToInt(char i[4]) {
+    return (i[0] << 21) | ((i[1] << 14) | ((i[2] << 7) | (i[3] | (int)0)));
+}
 
 int main(int argc, int *argv) {
     FILE *f = fopen("./testfile.mp3", "rb");
@@ -42,6 +53,8 @@ int main(int argc, int *argv) {
         exit(1);
     }
 
+    int metadata_size = synchsafeint32ToInt(header->size);
+
     printf("Header: \n");
     printf("\tFile Identifier: %c%c%c\n", header->fid[0], header->fid[1], header->fid[2]);
     printf("\tVersion: 2.%d.%d\n", header->ver[0], header->ver[1]);
@@ -50,8 +63,40 @@ int main(int argc, int *argv) {
     printf("\t\tExtended Header: %d\n", header->flags >> 6);
     printf("\t\tExp. Indicator: %d\n", header->flags >> 5);
     printf("\t\tFooter present: %d\n", header->flags >> 4);
-    printf("\tTag Size: %d\n", (header->size[0] << 21) | ((header->size[1] << 14) | ((header->size[2] << 7) | (header->size[3] | (int)0))));
+    printf("\tTag Size: %d\n", metadata_size);
     
+    int i = 0;
+
+    while (i < metadata_size) {
+        ID3V2_FRAME_HEADER frame_header;
+
+        fid_sz = fread(frame_header.fid, 1, 4, f);
+        if (fid_sz != 4) {
+            printf("Error occurred reading file identifier.\n");
+            exit(1);
+        }
+        size_sz = fread(frame_header.size, 1, 4, f);
+        if (size_sz != 4) {
+            printf("Error occurred tag size.\n");
+            exit(1);
+        }
+        flags_sz = fread(frame_header.flags, 1, 2, f);
+        if (flags_sz != 2) {
+            printf("Error occurred flags.\n");
+            exit(1);
+        }
+        int frame_data_sz = synchsafeint32ToInt(frame_header.size);
+
+        if (frame_data_sz > 0) { 
+            printf("FID: %.4s, ", frame_header.fid);
+            printf("Size: %d\n", frame_data_sz);
+        }
+
+        i += 10 + frame_data_sz;
+
+        fseek(f, frame_data_sz, SEEK_CUR);
+    }
+
     free(header);
     fclose(f);
     return 0;

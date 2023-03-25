09d401ec9b696f644db5ccff9b67b9b714426b3a
Alhumam Jabakhanji <alhumam567@gmail.com>
2023-03-25 11:54:56 -0400

    Refactoring

---
+++ mp3_metadata.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_INC
+#define HEADER_INC
+
+#include <stdio.h>
+
+typedef struct ID3V2_HEADER {
+    char fid[3];
+    char ver[2];
+    char flags;
+    char size[4];
+} ID3V2_HEADER;
+
+typedef struct ID3V2_FRAME_HEADER {
+    char fid[4];
+    char size[4];
+    char flags[2];
+} ID3V2_FRAME_HEADER;
+
+typedef struct ID3_METAINFO {
+    int metadata_sz;
+    int frame_count;
+    char (*fids)[4];
+} ID3_METAINFO;
+
+#endif
